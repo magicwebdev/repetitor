8257c2c4b8083710b584df7be0d558a2a828be3c
Александра Храпкова <axi83@mail.ru>
2021-10-30 13:33:04 +0300

    finish styling input type file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 $(function() {
+  // отслеживаем нажатие кнопки меню
   $(".header__toggle-btn").on('click', function(){
       $(".header__toggle-btn--1").toggleClass("active");
       $(".header__toggle-btn--2").toggleClass("active");
@@ -6,4 +7,13 @@ $(function() {
       $(".header__menu").toggleClass("active");
       $(".header__menu-btn").toggleClass("active");
   });
+
+  // отслеживаем изменение инпута file
+  $('.order__form-input--download').change(function(){
+    // если файл прикрепили то заносим значение value в переменную
+    let fileResult = $(this).val();
+    // и дальше передаем значение в инпут который под загрузчиком
+    $(this).parent().find('.order__form-input--file').val(fileResult);
+  });
+
 });
